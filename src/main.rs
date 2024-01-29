812fcff00aa53190e1ed5b76857b0945a4a10ea5
Marcelo Henrique Neppel <marcelo.neppel@gmail.com>
2024-01-28 22:16:31 -0300

    Add initial CLI
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+mod juju;
+
 fn main() {
-    println!("Hello, world!");
+    let engine = std::env::args()
+        .nth(1)
+        .expect("no engine given (should be one of: juju)");
+    let available_applications = match engine.as_str() {
+        "juju" => juju::list_applications(),
+        _ => {
+            println!("unknown engine: {}", engine);
+            std::process::exit(1);
+        }
+    };
+    let application = std::env::args().nth(2).expect("no application given");
+    if !available_applications.contains(&application) {
+        println!("unknown application: {}", application);
+        std::process::exit(1);
+    }
+    println!("engine: {:?}, application: {:?}", engine, application)
 }
